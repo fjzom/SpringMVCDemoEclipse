8df45d1205d5441c7d7bf8f9604f80ebe37695c3
Oviedo Maltez <felipe.oviedo.maltez@accenture.com>
2016-08-23 15:50:04 -0500

    add test class 2

---
+++ src/com/tutorialspoint/StudentControllerTest.java
@@ -0,0 +1,29 @@
+package com.tutorialspoint;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.ui.ModelMap;
+
+public class StudentControllerTest {
+
+	@Test
+	public void test() {
+		Student student = new Student();
+		ModelMap model = new ModelMap();
+		
+		
+		student.setName("lorem");
+		student.setId(12);
+		student.setAge(22);
+
+		model.addAttribute("name", student.getName());
+		model.addAttribute("id", student.getId());
+		model.addAttribute("age", student.getAge());
+		
+		
+		StudentController obj = new StudentController();
+		assertEquals("resuwlt", obj.addStudent(student, model));
+	}
+
+}
